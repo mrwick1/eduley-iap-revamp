f4ba80b46c0394b52493ccb1922b9e43cf57ca6f
Arjun KR <arjunkrishnaraj123@gmail.com>
2025-04-20 00:46:15 +0530

    feat: adding loading state for export excel

--- src/features/student-profile/components/primary-button.tsx
+++ src/features/student-profile/components/primary-button.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button';
+import { Spinner } from '@/components/ui/spinner';
 import { IconDownload } from '@tabler/icons-react';
 
 export const StudentProfilePrimaryButtons = ({
@@ -10,8 +11,9 @@ export const StudentProfilePrimaryButtons = ({
 }) => {
     return (
         <div className="flex gap-2 pb-2">
-            <Button variant="outline" className="space-x-1" onClick={handleDownloadExcel} loading={isDownloading}>
-                <span>Export Excel</span> <IconDownload size={18} />
+            <Button variant="outline" className="space-x-1" onClick={handleDownloadExcel} disabled={isDownloading}>
+                <span>Export Excel</span>
+                {isDownloading ? <Spinner size="sm" /> : <IconDownload size={18} />}
             </Button>
         </div>
     );
